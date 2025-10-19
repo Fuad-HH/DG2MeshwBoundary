0ca94ac4679f3d3c36d68d030f94ea0668d982dc
Fuad Hasibul Hasan <fuadhhasan.acc@gmail.com>
2025-10-19 13:33:17 -0400

    simmetrix not needed

--- config-rhel9.sh
+++ config-rhel9.sh
@@ -1,14 +1,7 @@
-#SIM_VER=simmetrix-simmodsuite-2024.1-240620dev-g4avy72vpgsyvhc4wkwbg4zh2kn4v74z
-#SIM_VER=simmetrix-simmodsuite-2025.0-241016dev-vafjs2qz5n2rokmrhs5ijvsua64ty4kz
-SIM_VER=simmetrix-simmodsuite-2025.1-250602dev-yv5oiomk2sdcru5cn5vqjxprycytc4ow
-SIM_ARCHOS=x64_rhel8_gcc83
-
 cmake -S . -B build \
 	-DSIM_MPI=mpich4.1.1 \
-	-DSIMMETRIX_INCLUDE_DIR=/opt/scorec/spack/rhel9/v0201_4/install/linux-rhel9-x86_64/gcc-12.3.0/$SIM_VER/include \
-	-DSIMMETRIX_LIB_DIR=/opt/scorec/spack/rhel9/v0201_4/install/linux-rhel9-x86_64/gcc-12.3.0/$SIM_VER/lib/$SIM_ARCHOS \
+	-DSIMMETRIX_INCLUDE_DIR=$SIMMETRIX_SIMMODSUITE_ROOT/include/ \
 	-DCMAKE_CXX_COMPILER=mpicxx \
 	-DCMAKE_C_COMPILER=mpicc \
-	-DSIM_PARASOLID=ON
 
 cmake --build build -j2
